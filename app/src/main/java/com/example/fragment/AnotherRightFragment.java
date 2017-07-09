d61ecdaa413bf4169b02cd058364b97f8cba8500
15291783517 <848671907@qq.com>
2017-07-09 19:15:39 +0800

    first commit

---
+++ app/src/main/java/com/example/fragment/AnotherRightFragment.java
@@ -0,0 +1,17 @@
+package com.example.fragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AnotherRightFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.another_right_fragment,container,false);
+        return view;
+    }
+}
